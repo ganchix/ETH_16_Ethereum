424e9c5e67dcceca2d3cbf0c3ba25bb7cbe3dbdb
rafa.rios <rafa.rios@2getherbank.com>
2017-10-14 16:23:57 +0200

    Work in progress with testing.

--- UNIT-4/splitterProject/test/splitter.js
+++ UNIT-4/splitterProject/test/splitter.js
@@ -6,12 +6,13 @@ contract('Splitter', function(accounts) {
     var alice = accounts[0];
     var bob = accounts[1];
     var carol = accounts[2];
-    var jenny = accounts[3];
-    var jhon = accounts[4];
+    var daniel = accounts[3];
+    var emma = accounts[4];
+
 
     var contribution = 80;
 
-    var partContribution = 99997844300000000040;
+    var partContribution = contribution / 2;
 
     beforeEach(function() {
 
@@ -22,41 +23,60 @@ contract('Splitter', function(accounts) {
     });
 
 
-    it("should be Alice", function() {
+    it("should be Alice the owner", function() {
 
-        return contract.alice({ from: alice })
-            .then(function(_alice) {
-                assert.strictEqual(alice, _alice, "Isn't Alice");
+        return contract.getOwner({ from: alice })
+            .then(function(owner) {
+                assert.strictEqual(alice, owner, "Owner Isn't Alice");
             });
 
     });
 
     it("should apply the split correctly to bob and carol when not pass addresses", function() {
+        var carolBalance;
 
         return contract.split(carol, bob, { from: alice, value: contribution })
             .then(function(tx) {
-            contract.withdraw({ from: carol })
-            .then(function() {
-                assert.equal(partContribution, web3.eth.getBalance(carol).toString(10), "The split is not correct");
-            contract.withdraw({ from: bob })
+                return web3.eth.getBalance(carol);
+            })
+            .then(function(balance) {
+                carolBalance = balance;
+                console.log("Carol Balance ", carolBalance);
+                return contract.withdraw({ from: carol });
+            })
+            .then(function(txnCarolWithDraw) {
+                var gasPrice = web3.eth.gasPrice;
+
+                console.log("Total gas ", txnCarolWithDraw.receipt.gasUsed);
+                console.log("partContribution ", partContribution);
+                console.log("txnCarolWithDraw ", txnCarolWithDraw);
+
+                var fee = gasPrice.times(txnCarolWithDraw.receipt.gasUsed);
+                console.log("fee ", fee);
+                var carolWithDrawBlance = carolBalance.plus(partContribution).minus(fee).toString(10);
+
+                console.log("carolWithDrawBlance ", carolWithDrawBlance);
+
+                assert.equal(carolWithDrawBlance, web3.eth.getBalance(carol).toString(10), "The split is not correct");
+                return contract.withdraw({ from: bob });
+            })
             .then(function() {
                 assert.equal(partContribution, web3.eth.getBalance(bob).toString(10), "The split is not correct");
             });
-            });
-            });
     });
+
     it("should apply the split correctly to addresses", function() {
 
-        return contract.split(jenny, jhon, { from: alice, value: contribution })
+        return contract.split(daniel, emma, { from: alice, value: contribution })
             .then(function(tx) {
-            contract.withdraw({ from: jenny })
+                return contract.withdraw({ from: daniel });
+            })
             .then(function() {
-                assert.equal(partContribution, web3.eth.getBalance(jenny).toString(10), "The split is not correct");
-            contract.withdraw({ from: jhon })
+                assert.equal(partContribution, web3.eth.getBalance(daniel).toString(10), "The split is not correct");
+                return contract.withdraw({ from: emma });
+            })
             .then(function() {
-                assert.equal(partContribution, web3.eth.getBalance(jhon).toString(10), "The split is not correct");
-            });
-            });
+                assert.equal(partContribution, web3.eth.getBalance(emma).toString(10), "The split is not correct");
             });
     });
 });
